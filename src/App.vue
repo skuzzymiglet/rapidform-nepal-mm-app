efa3e46855d87515f4131249812009ab16fdd934
skuzzymiglet <skuzzymiglet@gmail.com>
2020-03-29 11:28:56 +0100

    Basic (non-working) shot functionality, mock S3 upload

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="app">
         <vue-web-cam ref="cam" :selectFirstDevice="true"/>
-        <button v-on:click="send()">Honkass</button>
+            <button v-on:click="shoot()">Capture</button>
+            <button v-on:click="send()">Next Form</button>
     </div>
 </template>
 
@@ -9,16 +10,25 @@
 import Vue from 'vue'
 import { WebCam } from 'vue-web-cam';
 Vue.use(WebCam);
-import { base64ToBlob } from 'base64toblob';
 
 export default {
     name: 'app',
     components: {
         'vue-web-cam': WebCam,
     },
+    data: function() {
+        return {
+            shots: []
+        };
+    },
     methods: {
+        shoot: function(){
+            fetch(this.$refs.cam.capture()).then(res => res.blob).then(e => this.shots.push(e))
+            console.log(this.shots)
+        },
         send: function(){
-            console.log(this.$refs.cam.capture())
+            console.log("Sending shots to S3")
+            this.shots = []
         }
     }
 }
