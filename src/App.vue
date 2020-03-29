2646396bd7347f8368faf8ff8d7e93440b81f9d4
skuzzymiglet <skuzzymiglet@gmail.com>
2020-03-29 10:09:04 +0100

    Webcam

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
     <div id="app">
-        <vue-web-cam height="500" width="500"/>
+        <vue-web-cam ref="cam" :selectFirstDevice="true"/>
+        <button v-on:click="send()">Honkass</button>
     </div>
 </template>
 
 <script>
-    import Vue from 'vue'
-    import { WebCam } from 'vue-web-cam';
-    Vue.use(WebCam);
+import Vue from 'vue'
+import { WebCam } from 'vue-web-cam';
+Vue.use(WebCam);
+import { base64ToBlob } from 'base64toblob';
+
+export default {
+    name: 'app',
+    components: {
+        'vue-web-cam': WebCam,
+    },
+    methods: {
+        send: function(){
+            console.log(this.$refs.cam.capture())
+        }
+    }
+}
 </script>
